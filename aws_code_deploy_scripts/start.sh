bf78db2759265e6a6824998938df38bd631e8c7f
valexby <alexvasiuk@gmail.com>
2021-05-01 19:45:17 +0300

    Fix starting script

--- aws_code_deploy_scripts/start.sh
+++ aws_code_deploy_scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker run --detach --restart always -p5000:80
+sudo docker run --detach --restart always -p5000:80 api:latest
 # sudo cp /var/ops-ground/ops-ground.service /etc/systemd/system/
 # sudo systemctl enable ops-ground.service
 # sudo systemctl start ops-ground.service
